f798074aefe959ce2cc8d58b0e47dae67061c43c
namsick96 <namsick96@naver.com>
2021-04-05 23:21:31 +0900

    test2

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -4,10 +4,22 @@
 #include <stdlib.h>
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-    int aaa;
-    scanf("%d", &aaa);
-    printf("%d\n", aaa);
+    pid_t pid;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        printf("자식 프로세스\n");
+        int execvp(const char *file, char *const argv[]);
+        // exit(0);
+    }
+    if (pid > 0)
+    {
+        printf("Wait\n");
+        //wait(NULL);
+        printf("Exit\n");
+    }
     return 0;
 }
